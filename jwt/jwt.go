dae620344f7debd884968db2762856beab03dc13
Sergio Dominguez <sergio.dominguez@sonorasoft.com>
2021-09-04 21:26:30 -0700

    Login handler

--- jwt/jwt.go
+++ jwt/jwt.go
@@ -1,29 +1,29 @@
 package jwt
 
-import(
-	"time"
+import (
 	jwt "github.com/dgrijalva/jwt-go"
 	"github.com/lmSeryi/golang-twitter/models"
-) 
+	"time"
+)
 
 /* GenerateJWT genereate JWT encrypt */
-func GenerateJWT(t models.User) (string, error){
+func GenerateJWT(t models.User) (string, error) {
 	code := []byte("lmSeryiJuasJuas")
 	payload := jwt.MapClaims{
-		"_id": t.ID.Hex(),
-		"email" : t.Email,
-		"name" : t.Name,
-		"lastName" : t.LastName,
-		"birthday" : t.Birthday,
-		"biography" : t.Biography,
-		"location" : t.Location,
-		"webSite" : t.WebSite,
-		"exp": time.Now.Add(time.Hour * 24).Unix(),
+		"_id":       t.ID.Hex(),
+		"email":     t.Email,
+		"name":      t.Name,
+		"lastName":  t.LastName,
+		"birthday":  t.Birthday,
+		"biography": t.Biography,
+		"location":  t.Location,
+		"webSite":   t.WebSite,
+		"exp":       time.Now().Add(time.Hour * 24).Unix(),
 	}
 	token := jwt.NewWithhClaims(jwt.SigninMethodHS256, payload)
 	tokenStr, err := token.SignedString(code)
-	
-	if err != nil{
+
+	if err != nil {
 		return token, err
 	}
 	return tokenStr, nil
