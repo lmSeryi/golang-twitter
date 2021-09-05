bbf54984dacf1a58845e9a503c9aa148ab5cd0ad
Sergio Dominguez <sergio.dominguez@sonorasoft.com>
2021-09-04 21:25:07 -0700

    Login handler

--- jwt/jwt.go
+++ jwt/jwt.go
@@ -18,11 +18,11 @@ func GenerateJWT(t models.User) (string, error){
 		"biography" : t.Biography,
 		"location" : t.Location,
 		"webSite" : t.WebSite,
-		"exp": time.Now.Add(time.Hour * 24).Unix()
+		"exp": time.Now.Add(time.Hour * 24).Unix(),
 	}
 	token := jwt.NewWithhClaims(jwt.SigninMethodHS256, payload)
 	tokenStr, err := token.SignedString(code)
-
+	
 	if err != nil{
 		return token, err
 	}
