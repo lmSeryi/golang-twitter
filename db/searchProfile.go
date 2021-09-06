f2b68af2e798ff9ad234c48453e071bc98a14ec7
Sergio Dominguez <sergio.dominguez@sonorasoft.com>
2021-09-05 22:29:06 -0700

    Profile

---
+++ db/searchProfile.go
@@ -0,0 +1,36 @@
+package db
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/primitive"
+
+	"github.com/lmSeryi/golang-twitter/models"
+)
+
+func SearchProfile(Id string)(models.User, error){
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second *15)
+	defer cancel()
+
+	db := MongoCN.Database("twitter")
+	col := db.Collection("users")
+
+	var profile models.User
+	objId, _ := primitive.ObjectIDFromHex(Id)
+
+	condition := bson.M{
+		"_id": objId,
+	}
+
+	err := col.FindOne(ctx, condition).Decode(&profile)
+	profile.Password = ""
+
+	if err != nil{
+		fmt.Println("Profile not found " + err.Error())
+		return profile, err
+	}
+	return profile, nil
+}
--- routers/profile.go
+++ routers/profile.go
@@ -1,5 +1,26 @@
 package routers
 
-func Profile() {
+import (
+	"encoding/json"
+	"net/http"
+	"github.com/lmSeryi/golang-twitter/db"
+)
 
+func Profile(w http.ReponseWriter, r *http.Request) {
+	Id := r.URL.Query().Get("id")
+
+	if len(Id) < 1{
+		http.Error(w, "Invalid Id", 400)
+		return
+	}
+
+	profile, err := db.SearchProfile(Id)
+
+	if err != nil{
+		http.Error(w, "Error while searching profile " + err.Error(), 400)
+		return
+	}
+	w.Header().Set("Context-Type", "application/json")
+	w.Writeheader(http.StatusCreated)
+	json.NewEncoder(w).Encode(profile)
 }
